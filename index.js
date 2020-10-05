5c5ca2c39425b21c22f0e548a70f07369ad282ec
dev-zam <zamsyh.work@gmail.com>
2020-10-05 10:45:03 +0700

    add new endpoint

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const cors = require('cors');
 const http = require('http');
+const conn = require('./env/index');
 
 const app = express();
+const insert = require('./view/insert');
+const find = require('./view/find');
 
 app.use(express.json());
 app.use(cors());
 
+
+app.use("/api/counseling", insert);
+app.use("/api/counseling", find);
 app.all( "*", (req, res)=>{res.send({res:"Fuck you!!"});} );
 
 const server = http.createServer(app);
-server.listen(8888);
+server.listen(conn.PORT);
