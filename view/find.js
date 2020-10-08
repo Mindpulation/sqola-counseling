2fa584406f09a0d3405fe64ead38e22b2b081995
dev-zam <zamsyh.work@gmail.com>
2020-10-08 14:39:08 +0700

    update the router find to add joi

--- view/find.js
+++ view/find.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express.Router();
 
+const {findList} = require('../validator/index');
+
 const { findRoomId } = require('../controller/mongo');
 
-app.get('/find', findRoomId);
+app.get('/find', findList, findRoomId);
 
 module.exports = app;
